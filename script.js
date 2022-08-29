021798549b3ac58f69ca97fc354bae2d15686301
Richard Vega <rjproa@gmail.com>
2022-08-29 18:03:36 -0500

    Prueba de Progressive web App a la aplicación de notas

--- script.js
+++ script.js
@@ -218,6 +218,12 @@ const draw = () => {
   $phrase.textContent = `"${winner}"`;
 }
 
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./sw.js')
+    .then(reg => console.log('Registro de SW exitoso', reg))
+    .catch(err => console.warn('Error al tratar de registrar el SW', err));
+}
+
 d.addEventListener("DOMContentLoaded", (e) => {
   if (ls.getItem("user") === null) formValidation();
   titleEdit();
